b61a75a6215523ca5705abbbfb06d5928036f71c
Ivan <klimash160@gmail.com>
2021-08-12 18:07:28 +0300

    aded hamburger menu

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 // slick slider
-
 $(".slider-block").slick({
-  autoplay: false,
+  autoplay: true,
   dots: true,
   dotsClass: true,
   draggable: false,
   swipe: false,
   touchMove: false,
+});
+
+// Гамбургер меню
+let mobileMenu = document.querySelector(".nav-mobile-menu");
+let mainMenu = document.querySelector(".menu-wrapper");
+let buttonBuy = document.querySelector(".right-column");
+
+// console.log(mobileMenu);
+// console.log(mainMenu);
+// console.log(buttonBuy);
+
+mobileMenu.addEventListener("click", function () {
+  mobileMenu.classList.toggle("active-menu");
+  if (mobileMenu.classList.contains("active-menu")) {
+    mainMenu.classList.add("active-menu") ||
+      buttonBuy.classList.add("active-menu");
+  } else {
+    mainMenu.classList.remove("active-menu") ||
+      buttonBuy.classList.remove("active-menu");
+  }
 });
